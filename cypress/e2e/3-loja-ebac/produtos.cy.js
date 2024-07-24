3a47e5a22cd75a26c15ee5528abacbaa4dd524d7
João Matheus <joao_taiko@hotmail.com>
2024-07-24 13:14:54 -0300

    Usando pages

--- cypress/e2e/3-loja-ebac/produtos.cy.js
+++ cypress/e2e/3-loja-ebac/produtos.cy.js
@@ -1,20 +1,29 @@
 /// <reference types="cypress"/>
+import produtosPage from "../../support/page-objects/produtos.page";
 
 describe('Funcionalidade: Produtos', () => {
 
     beforeEach(() => {
-        cy.visit('produtos')
+        produtosPage.visitarUrl()
     });
     it('Deve selecionar um preoduto da lista', () => {
+        let produto = 'Abominable Hoodie'
+        produtosPage.buscarProdutoLista(produto)
+        cy.get('.product_title').should('contain', produto)
+        
+    });
 
-        cy.get('.product-block')
-            //.first()
-            //.last()
-            //.eq(2)
-            .contains('Ariel Roll Sleeve Sweatshirt')
-            .click()
+    it.only('Deve buscar um produto com sucesso', () => {
+        produtosPage.buscarProduto('Aether Gym Pant')
+        cy.get('.product_title').should('contain', 'Aether Gym Pant')
+        
+    });
 
-            cy.get('.product_title').should('contain', 'Ariel Roll Sleeve Sweatshirt')
+    it('Deve visitar a página de produto', () => {
+        
+    });
+
+    it('Deve adicionar produto ao carrinho', () => {
         
     });
 });
